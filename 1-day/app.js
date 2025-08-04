13cb40b97e309517266ff2bc71133dbb932208ea
BahromWeb <bahromhasanov0202@gmail.com>
2025-08-04 21:16:37 +0500

    added new examples

---
+++ 1-day/app.js
@@ -0,0 +1,29 @@
+// JavaScript masala chelenge
+
+// 1-masala
+// Berilgan massivdagi eng katta elementni toping.
+// Input: [5, 9, 2, 4, 11, 3]
+// Output: 11
+
+// const findMax=(arr)=>{
+//      max=arr[0]
+//      for(let i=1; i<arr.length; i++){
+//         if(arr[i]>max){
+//             max=arr[i]
+//         }
+//      }
+//      return (max)
+// }
+
+// console.log(findMax([5, 9, 2, 4, 11, 3]))
+
+// method usul
+
+// const findMax=(arr)=>{
+//     return Math.max(...arr)
+// }
+
+// console.log(findMax([5, 9, 2, 4, 11, 3]))
+
+// 2-masala 
+
