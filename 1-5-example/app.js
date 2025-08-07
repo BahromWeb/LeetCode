8d450c92781b79f747eab131fc7c39afe500c5c4
BahromWeb <bahromhasanov0202@gmail.com>
2025-08-07 21:20:49 +0500

    1-example

---
+++ 1-5-example/app.js
@@ -0,0 +1,9 @@
+// LeetCode oson masalalari JavaScript bilan
+
+// 1. Two Sum
+// 🔹 Masala:
+// Sizga sonlardan iborat nums array va target soni berilgan.
+// Shunday ikkita index topingki, ularning qiymatlari yig'indisi targetga teng bo‘lsin.
+
+
+
