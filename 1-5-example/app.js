114ccf52693eff91bc4f8f4594e31dfc15bd39c0
BahromWeb <bahromhasanov0202@gmail.com>
2025-08-10 20:35:26 +0500

    added new example

--- 1-5-example/app.js
+++ 1-5-example/app.js
@@ -135,3 +135,15 @@
 // Vazifa — ularning eng uzun umumiy prefiksini topish.
 // Agar umumiy prefiks bo‘lmasa, bo‘sh string ("") qaytar.
 
+const longestCommonPrefix=(strs)=>{
+    if (!strs.length) return "";
+    let prefix = strs[0];
+    for (let i = 1; i < strs.length; i++) {
+        while (strs[i].indexOf(prefix) !== 0) {
+            prefix = prefix.slice(0, prefix.length - 1);
+            if (!prefix) return "";
+        }
+    }
+    return prefix;
+}
+console.log(longestCommonPrefix(["flower", "flow", "flight"]))
