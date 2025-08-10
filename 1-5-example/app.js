588a825c28398db451b1a0bd166f5be57d6d3cb9
BahromWeb <bahromhasanov0202@gmail.com>
2025-08-10 10:38:55 +0500

    added new example

--- 1-5-example/app.js
+++ 1-5-example/app.js
@@ -34,3 +34,52 @@
 
 // console.log(twoSum([3, 4, 5, 7, 8, 89, 43, 20], 15))
 
+// 2-masala
+// berilgan butun son x palindrom bo‘lsa true qaytar, bo‘lmasa false qaytar.
+// Palindrom — oldindan va orqadan bir xil o‘qiladigan son.
+
+// const isPalindrom=(x)=>{
+//     let str=x.toString()
+//     return str===str.split('').reverse().join('')
+// }
+
+// console.log(isPalindrom(1221))
+
+
+// way -2
+
+// const isPalindrom = (x) => {
+//     if (x < 0) return false;
+
+//     let reversed = 0; 
+//     let original = x; 
+
+//     while (x > 0) {
+//         reversed = reversed * 10 + (x % 10);
+//         x = Math.floor(x / 10);
+//     }
+
+//     if (reversed === original) {
+//         return `${original} Palindrom son`;
+//     } else {
+//         return `${original} Palindrom son emas`;
+//     }
+// }
+
+// console.log(isPalindrom(1221)); 
+// console.log(isPalindrom(1234)); 
+
+// way-3
+
+// function isPalindrome(x) {
+//     if (x < 0 || (x % 10 === 0 && x !== 0)) return false;
+//     let reversedHalf = 0;
+//     while (x > reversedHalf) {
+//         reversedHalf = reversedHalf * 10 + x % 10;
+//         x = Math.floor(x / 10);
+//     }
+//     return x === reversedHalf || x === Math.floor(reversedHalf / 10);
+// }
+// console.log(isPalindrome(121));
+
+
