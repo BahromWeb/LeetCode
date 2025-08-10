e8fe152b63d0ba8e21de4b8efdd9c172f428525d
BahromWeb <bahromhasanov0202@gmail.com>
2025-08-10 10:50:59 +0500

    added new example

--- 1-5-example/app.js
+++ 1-5-example/app.js
@@ -83,3 +83,33 @@
 // console.log(isPalindrome(121));
 
 
+// 3-masala: Roman to Integer
+//  rim raqami (roman numeral) ko‘rinishidagi matn beriladi.
+// Vazifa — uni butun songa aylantirib qaytarish.
+
+// I → 1  
+// V → 5  
+// X → 10   => Matematik eslatma
+// L → 50  
+// C → 100  
+// D → 500  
+// M → 1000
+
+// const romanToInt=(s)=>{
+//     const values = {
+//         I: 1, V: 5, X: 10, L: 50,
+//         C: 100, D: 500, M: 1000
+//     }
+
+//     let total=0;
+//     for(let i=0; i<s.length; i++){
+//         if(values[s[i]]<values[s[i+1]]){
+//             total-=values[s[i]]
+//         }else{
+//             total+=values[s[i]]
+//         }
+//     }
+//     return total
+// }
+
+// console.log(romanToInt("XXMMDD"))
