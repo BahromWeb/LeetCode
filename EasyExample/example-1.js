0d54068abaa2fb45eeff01db32d8c98b22e3864f
BahromWeb <bahromhasanov0202@gmail.com>
2025-11-29 13:02:09 +0500

    added new example leetCode

---
+++ EasyExample/example-1.js
@@ -0,0 +1,37 @@
+// LeetCode ID: 1
+// Difficulty: Easy
+// Masala sharti:
+// Sizga integer array nums va integer target berilgan. Sizga ikki raqamni topish kerak, ularning yig‘indisi targetga teng bo‘lsin. Shu ikki raqamning indekslarini return qilishingiz kerak.
+// Siz har doim faqat bitta yechim mavjudligini taxmin qilasiz va bir xil elementni ikki marta ishlatib bo‘lmaydi.
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: nums[0] + nums[1] = 2 + 7 = 9
+
+// const twoSum =(nums, target) => {
+//     for(let i = 0; i < nums.length; i++) {
+//         for(let j = i + 1; j < nums.length; j++) {
+//             if(nums[i] + nums[j] === target) {
+//                 return [i, j];
+//             }
+//         }
+//    }
+// }
+
+// console.log(twoSum([2,7,11,15], 9)); // Output: [0, 1]
+
+// Two way
+
+const twoSum=(nums, target) => {
+    const numMap = new Map();
+    for(let i=0; i<nums.length; i++){
+        const complement = target - nums[i];
+        if(numMap.has(complement)){
+            return [numMap.get(complement), i];
+        }
+        numMap.set(nums[i], i);
+    }
+}
+
+console.log(twoSum([2,7,11,15], 9)); // Output: [0, 1]
+
